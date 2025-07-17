9481ba7722e9a0845431b558a9a42de0a981cbdf
Ming <2847919699@qq.com>
2025-07-17 16:23:47 +0800

    删除菜品功能

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -2,12 +2,16 @@ package com.sky.service.impl;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.sky.constant.MessageConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
+import com.sky.exception.DeletionNotAllowedException;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealDishMapper;
 import com.sky.result.PageResult;
 import com.sky.service.DishService;
 import com.sky.vo.DishVO;
@@ -27,7 +31,8 @@ public class DishServiceImpl implements DishService {
     private DishMapper dishMapper;
     @Autowired
     private DishFlavorMapper dishFlavorMapper;
-
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
     /**
      * 新增菜品，同时保存对应的口味数据
      *
@@ -43,11 +48,16 @@ public class DishServiceImpl implements DishService {
         //向菜品表插入1条数据
         dishMapper.insert(dish);
 
+        //获取insert语句生成的主键值
         Long dishId = dish.getId();
 
         List<DishFlavor> flavors = dishDTO.getFlavors();
         if (flavors != null && flavors.size() > 0) {
             //向口味表插入n条数据
+            flavors.forEach(dishFlavor -> {
+                    dishFlavor.setDishId(dishId);
+                    });
+
             dishFlavorMapper.insertBatch(flavors);
         }
     }
@@ -63,5 +73,51 @@ public class DishServiceImpl implements DishService {
         Page<DishVO> page = dishMapper.pageQuery(dishPageQueryDTO);
 
         return new PageResult(page.getTotal(),page.getResult());
+    }
+
+//    @Override
+//    public void delete(List<Long> ids) {
+//        ids.forEach(id -> {
+//            Dish dish = dishMapper.getById(id);
+//            if (dish.getStatus() == 1) {
+//                //起售中的菜品不能删除
+//                throw new RuntimeException("起售中的菜品不能删除");
+//            }
+//        });
+//        //批量删除菜品数据
+//        dishMapper.delete(ids);
+//        //批量删除菜品对应的口味数据
+//        dishFlavorMapper.deleteByDishId(ids);
+//    }
+
+    /**
+     * 菜品批量删除
+     * @param ids
+     */
+    @Transactional
+    public void deleteBatch(List<Long> ids){
+        //判断当前菜品是否能够删除 -- 存在起售中的菜品？？
+        for(Long id: ids){
+            Dish dish =dishMapper.getById(id);
+            if (dish.getStatus() == StatusConstant.ENABLE){
+                //当前菜品属于起售中，不能删除
+                throw new DeletionNotAllowedException(MessageConstant.DISH_ON_SALE);
+            }
+        }
+
+        //判断当前菜品是否能够删除 -- 是否被套餐关联了
+        List<Long>setmealIds =setmealDishMapper.getSetmealIdsByDishIds(ids);
+        if(setmealIds !=null && setmealIds.size()>0){
+            //当前菜品被套餐关联了，不能删除了
+            throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
+        }
+
+        //删除菜品表中的菜品数据
+        for(Long id :ids){
+            dishMapper.deleteById(id);
+            //删除菜品关联的口味数据
+            dishFlavorMapper.deleteByDishId(id);
+        }
+
     }
 }
